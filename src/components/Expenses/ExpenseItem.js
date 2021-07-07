3749fd33c74b63fcba26840f426be9fb292a9634
Darryl Karney <karneyd@outlook.com>
2021-07-06 19:31:12 -0500

    Lesson 48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,14 @@
+import {useState} from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = "Updated!";
+    setTitle('Updated!');
   };
 
   return (
